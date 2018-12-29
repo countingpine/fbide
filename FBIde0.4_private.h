abf1d5fea294a200a010663861b04bb04b6b7de0
Matthew Fearnley <matthew.w.fearnley@gmail.com>
2018-12-29 15:09:11 +0000

    Wrap more literal strings in '_T()'
    sed -i 's/"[^"]*"/_T(&)/g' FBIde0.4_private.h

--- FBIde0.4_private.h
+++ FBIde0.4_private.h
@@ -5,19 +5,19 @@
 #define FBIDE0_4_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.4.4.0"
+#define VER_STRING	_T("0.4.4.0")
 #define VER_MAJOR	0
 #define VER_MINOR	4
 #define VER_RELEASE	4
 #define VER_BUILD	0
-#define COMPANY_NAME	""
-#define FILE_VERSION	"0.4.4"
-#define FILE_DESCRIPTION	"IDE for FreeBasic compiler"
-#define INTERNAL_NAME	"fbide"
-#define LEGAL_COPYRIGHT	"Released under GPL"
-#define LEGAL_TRADEMARKS	""
-#define ORIGINAL_FILENAME	"fbide.exe"
-#define PRODUCT_NAME	"FBIde"
-#define PRODUCT_VERSION	"0.4.4"
+#define COMPANY_NAME	_T("")
+#define FILE_VERSION	_T("0.4.4")
+#define FILE_DESCRIPTION	_T("IDE for FreeBasic compiler")
+#define INTERNAL_NAME	_T("fbide")
+#define LEGAL_COPYRIGHT	_T("Released under GPL")
+#define LEGAL_TRADEMARKS	_T("")
+#define ORIGINAL_FILENAME	_T("fbide.exe")
+#define PRODUCT_NAME	_T("FBIde")
+#define PRODUCT_VERSION	_T("0.4.4")
 
 #endif /*FBIDE0_4_PRIVATE_H*/
